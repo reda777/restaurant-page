296f1990944fa36bea30de9af6e82c1042084919
reda777 <redazara1998@gmail.com>
2022-05-04 11:41:14 +0000

    Design navigation bar

--- src/index.js
+++ src/index.js
@@ -2,40 +2,48 @@ import './style.css';
 import homePage from './homePage';
 import menuPage from './menuPage';
 import contactPage from './contactPage';
-//tab object 
-const tabObject = {
-    home: homePage(),
-    menu : menuPage(),
-    contact : contactPage()
-};
-//content div
-const contentDiv=document.createElement("div");
-contentDiv.setAttribute("id","content");
-document.body.appendChild(contentDiv);
+const mainPage = (() => {
+    //tab object 
+    const tabObject = {
+        home: homePage(),
+        menu: menuPage(),
+        contact: contactPage()
+    };
+    //content div
+    const contentDiv = document.createElement("div");
+    contentDiv.setAttribute("id", "content");
 
-//home tab
-const homeTab=document.createElement("div");
-homeTab.classList.add("tab");
-homeTab.textContent="Home";
-document.body.appendChild(homeTab);
+    //tab div
+    const tabDiv = document.createElement("div");
+    tabDiv.setAttribute("id", "tab");
 
-//menu tab
-const menuTab=document.createElement("div");
-menuTab.classList.add("tab");
-menuTab.textContent="Menu";
-document.body.appendChild(menuTab);
+    //home tab
+    const homeTab = document.createElement("div");
+    homeTab.classList.add("tab");
+    homeTab.textContent = "Home";
 
-//contact tab
-const contactTab=document.createElement("div");
-contactTab.classList.add("tab");
-contactTab.textContent="Contact";
-document.body.appendChild(contactTab);
+    //menu tab
+    const menuTab = document.createElement("div");
+    menuTab.classList.add("tab");
+    menuTab.textContent = "Menu";
 
-//navigation listeners
-document.querySelectorAll(".tab").forEach((tab)=>{
-    tab.addEventListener("click",()=>{
-        contentDiv.removeChild(contentDiv.firstChild);
-        contentDiv.appendChild(tabObject[tab.textContent.toLowerCase()]);
+    //contact tab
+    const contactTab = document.createElement("div");
+    contactTab.classList.add("tab");
+    contactTab.textContent = "Contact";
+    
+    document.body.appendChild(contentDiv);
+        contentDiv.appendChild(tabDiv);
+            tabDiv.appendChild(homeTab);
+            tabDiv.appendChild(menuTab);
+            tabDiv.appendChild(contactTab);
+        contentDiv.appendChild(tabObject.home);
+
+    //navigation listeners
+    document.querySelectorAll(".tab").forEach((tab) => {
+        tab.addEventListener("click", () => {
+            contentDiv.removeChild(contentDiv.lastChild);
+            contentDiv.appendChild(tabObject[tab.textContent.toLowerCase()]);
+        });
     });
-});
-contentDiv.appendChild(tabObject.home);
+})();
--- src/menuPage.js
+++ src/menuPage.js
@@ -1,17 +1,28 @@
-function menuPage(){
-    const backgroundImage=document.createElement("div");
-    backgroundImage.classList.add("backgroundImage");
-    const headline=document.createElement("div");
+function homePage() {
+    //home div--parent
+    const home = document.createElement("div");
+    home.classList.add("home");
+    //headline--1st child
+    const headline = document.createElement("div");
     headline.classList.add("headline");
-    headline.textContent="Menu",
-    backgroundImage.appendChild(headline);
-    const about=document.createElement("div");
+    headline.textContent = "Menu";
+    
+   //about--2nd child
+    const about = document.createElement("div");
     about.classList.add("about");
-    about.textContent=`*********** is the story of the ***** family, and it is based on love and collaboration within the family.
+    about.textContent = `*********** is the story of the ***** family, and it is based on love and collaboration within the family.
     The restaurant is a gourmet's dream come true.
     Its origins may be traced back to the year ****, when ******** ***** returned to ******* after 5 years abroad and obtained great knowledge.
     *** Restaurant was the first ******* restaurant to be named to the World's 50 Best Restaurants, thanks to its chef ***********. `;
-    backgroundImage.appendChild(about);
-    return backgroundImage;
+    
+    //background--3rd child
+    const backgroundImage=document.createElement("div");
+    backgroundImage.classList.add("backgroundImage");
+
+    home.appendChild(headline);
+    home.appendChild(about);
+    home.appendChild(backgroundImage);
+  
+    return home;
 }
-export default menuPage;
+export default homePage;
